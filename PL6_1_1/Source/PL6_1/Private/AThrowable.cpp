a1d6582e03d96b2cbec241ed02dac290686506f6
Velux28 <velutimatteo@gmail.com>
2024-10-26 12:00:08 +0200

    Start Throwable

---
+++ PL6_1_1/Source/PL6_1/Private/AThrowable.cpp
@@ -0,0 +1,60 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AThrowable.h"
+#include "Components/AudioComponent.h"
+#include "Kismet/GameplayStatics.h"
+
+// Sets default values
+AAThrowable::AAThrowable()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void AAThrowable::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	for(auto & Elem : SoundsMap)
+	{
+		Elem.Value = CreateDefaultSubobject<USoundBase>(*Elem.Key);
+	}
+}
+
+// Called every frame
+void AAThrowable::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+USoundBase* AAThrowable::AddSound(FString _SoundType, USoundBase* _NewSound)
+{
+	if (!_NewSound)
+	{
+		return NULL;
+	}
+	SoundsMap.Add(_SoundType, _NewSound);
+
+	SoundsMap[_SoundType] = CreateDefaultSubobject<USoundBase>(*_SoundType);
+
+	return _NewSound;
+}
+
+void AAThrowable::PlaySoundByName(FString _SoundType)
+{
+	if (!SoundsMap.Find(_SoundType))
+	{
+		UGameplayStatics::PlaySoundAtLocation(GetWorld(), DefaultSound, GetActorLocation());
+	}
+
+	UGameplayStatics::PlaySoundAtLocation(GetWorld(), SoundsMap[_SoundType], GetActorLocation());
+}
+
+void AAThrowable::PlaySoundWithParams(USoundBase* _Sound, FVector SoundLocation)
+{
+	UGameplayStatics::PlaySoundAtLocation(GetWorld(), _Sound, SoundLocation);
+}
+
---
+++ PL6_1_1/Source/PL6_1/Public/AThrowable.h
@@ -0,0 +1,43 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "AThrowable.generated.h"
+
+
+UCLASS()
+class PL6_1_API AAThrowable : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AAThrowable();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Sound")
+	TMap<FString, USoundBase*> SoundsMap;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Sound")
+		USoundBase* DefaultSound;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	UFUNCTION(BlueprintCallable)
+	USoundBase* AddSound(FString _SoundType, USoundBase* _NewSound);
+
+	UFUNCTION(BlueprintCallable)
+		void PlaySoundByName(FString _SoundType);
+
+	UFUNCTION(BlueprintCallable)
+		void PlaySoundWithParams(USoundBase* _Sound, FVector SoundLocation);
+
+};
