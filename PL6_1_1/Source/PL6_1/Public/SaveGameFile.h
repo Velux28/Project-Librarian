339a34a607fed016531a4b50b8ac3ac5d66ca2e4
Velux28 <velutimatteo@gmail.com>
2025-05-19 20:07:45 +0200

    Inizio nuovo save system

---
+++ PL6_1_1/Source/PL6_1/Public/SaveGameFile.h
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/SaveGame.h"
+#include "AThrowable.h"
+#include "SaveGameFile.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PL6_1_API USaveGameFile : public USaveGame
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Player")
+	FTransform PlayerTransform;
+
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Player")
+	int32 PlayerItems;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Level")
+	FName CurrLevel;
+};
